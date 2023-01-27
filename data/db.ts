5c8ee0224b838bf64257934db5dfc26d312bd89c
Sukirno <mblonyox@gmail.com>
2023-01-27 08:00:28 +0700

    fix: readSync unsupported, use mock_file

--- data/db.ts
+++ data/db.ts
@@ -1,9 +1,12 @@
 import { dirname, fromFileUrl, join } from "$std/path/mod.ts";
 import { DB } from "$sqlite/mod.ts";
-
+import { prepareLocalFile, prepareVirtualFile } from "$mock_file/mod.ts";
 const filepath = join(
   dirname(fromFileUrl(import.meta.url)),
   "./database.sqlite",
 );
+
+await prepareLocalFile(filepath);
+prepareVirtualFile(filepath + "-journal");
 
 export const db = new DB(filepath, { mode: "read" });
