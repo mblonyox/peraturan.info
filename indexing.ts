ff606be13c7e652a2b98fd3b2f1a0eeb7a891376
Sukirno <sukirno89@kemenkeu.go.id>
2023-02-23 18:45:17 +0700

    build; generate algolia indexing task

---
+++ indexing.ts
@@ -0,0 +1,17 @@
+import { getDB } from "@data/db.ts";
+import { getListPeraturan } from "@models/peraturan.ts";
+
+const db = await getDB();
+const { hasil } = await getListPeraturan(db, { pageSize: 20000 });
+
+const indexes = hasil.map((p) => ({
+  objectID: p.path,
+  jenis: p.namaJenisPanjang,
+  nomor: p.nomorPanjang,
+  judul: p.judul,
+  tanggal: p.tanggal_ditetapkan.getTime(),
+  partial: "rujuk_panjang",
+  content: p.rujukPanjang,
+}));
+
+Deno.writeTextFile("index.json", JSON.stringify(indexes));
