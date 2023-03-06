006391b8a5273a98de87e7a1a9535184c9fd4601
Sukirno <sukirno89@kemenkeu.go.id>
2023-03-06 19:10:15 +0700

    fix: add anonymous comment link

--- islands/webmentions.tsx
+++ islands/webmentions.tsx
@@ -1,6 +1,11 @@
 import { asset } from "$fresh/runtime.ts";
+import { useEffect, useState } from "preact/hooks";
 
 export default function Webmentions() {
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    setUrl(document.URL);
+  }, []);
   return (
     <>
       <link rel="stylesheet" href={asset("/styles/webmention.css")} />
@@ -13,6 +18,16 @@ export default function Webmentions() {
             <i>like</i>, <i>retweet/repost</i> pada <i>tweet</i>{" "}
             yang mencantumkan tautan pada laman ini.
           </p>
+          <div className="d-grid">
+            <a
+              href={`https://quill.p3k.io/?dontask=1&me=https://commentpara.de/&reply=${url}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+            >
+              Tulis tanggapan.
+            </a>
+          </div>
           <div id="webmentions"></div>
         </div>
       </div>
