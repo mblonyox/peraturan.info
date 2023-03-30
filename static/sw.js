dfa68b29c579fc89ffafd895f00c47c63fe557e1
Sukirno <mblonyox@gmail.com>
2023-03-31 01:38:29 +0700

    fix: add cdn & image cache sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,9 @@
 const CACHE = "peraturan-cache-v1.0";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
+);
+// import("https://esm.sh/workbox-sw@6.5.4");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -9,8 +12,22 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  "https://cdn.jsdelivr.net/(.*)",
+  new workbox.strategies.CacheFirst({
+    cacheName: "cdn-cache",
+  }),
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst({
+    cacheName: "images-cache",
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp("/*"),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
+    cacheName: CACHE,
+  }),
 );
