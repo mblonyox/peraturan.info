cc81674bf0ff22b5123da35f48c7ce9990175deb
Sukirno <mblonyox@gmail.com>
2023-06-07 01:27:34 +0700

    fix: ellipsis cutoff last word.

--- utils/string.ts
+++ utils/string.ts
@@ -1,5 +1,8 @@
 export const ellipsis = (text: string, maxLength = 80) => {
-  const subtext = text.trimEnd().substring(0, maxLength);
+  const trimmed = text.trim();
+  if (trimmed.length <= maxLength) return trimmed;
+  const subtext = trimmed.substring(0, maxLength);
   const index = subtext.lastIndexOf(" ");
-  return subtext.substring(0, index) + (text.length > maxLength ? "…" : "");
+  return subtext.substring(0, index) +
+    (trimmed.length > maxLength ? "…" : "");
 };
