503bf5d062176c976f9fc7bc9f56e73a37e29d0a
Sukirno <mblonyox@gmail.com>
2023-06-12 14:27:42 +0700

    fix: remove default json-ld from SeoTags

--- components/seo_tags.tsx
+++ components/seo_tags.tsx
@@ -14,35 +14,19 @@ export default function SeoTags() {
       <meta name="description" content={description} />
       <meta itemProp="name" content={title} />
       <meta itemProp="description" content={description} />
+      <meta itemProp="image" content={image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:url" content={url?.toString()} />
       <meta name="twitter:creator" content="@mblonyox" />
+      <meta name="twitter:image" content={image} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url?.toString()} />
       <meta property="og:site_name" content="Peraturan.deno.dev" />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "http://schema.org",
-            "@type": "WebSite",
-            name: title,
-            about: description,
-            url: url?.toString(),
-          }),
-        }}
-      />
-      {!!image && (
-        <>
-          <meta itemProp="image" content={image} />
-          <meta name="twitter:image" content={image} />
-          <meta property="og:image" content={image} />
-        </>
-      )}
+      <meta property="og:image" content={image} />
     </Head>
   );
 }
