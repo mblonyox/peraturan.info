a6c1ceede41af46ab312d27eeafa47e8cfaf3ba0
Sukirno <mblonyox@yahoo.co.id>
2023-09-13 21:12:47 +0700

    update deno fresh

--- main.ts
+++ main.ts
@@ -7,5 +7,6 @@ import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-await start(manifest);
+await start(manifest, config);
