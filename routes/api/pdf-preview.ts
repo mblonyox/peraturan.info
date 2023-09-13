903c1e632e3d082facac764d2afd5c5d4d91bcda
Sukirno <mblonyox@yahoo.co.id>
2023-09-14 04:31:56 +0700

    fix: refactor & pdfium heap cleanup

--- routes/api/pdf-preview.ts
+++ routes/api/pdf-preview.ts
@@ -6,18 +6,8 @@ export const handler: Handlers = {
   GET: async (req, _ctx) => {
     const url = new URL(req.url).searchParams.get("url");
     if (!url) throw new Error("Parameter 'url' not provided.");
-    const data = await fetch(url).then((res) => {
-      if (!res.ok) throw new Error(`Invalid url: ${res.statusText}`);
-      const contentType = res.headers.get("Content-Type");
-      if (contentType !== "application/pdf") {
-        throw new Error("Not a pdf document.");
-      }
-      return res.arrayBuffer();
-    });
-    const doc = new Document(new Uint8Array(data));
-    const page = doc.getPage(0);
-    const imageData = page?.getImageData();
-    if (!imageData) throw new Error("No Image found.");
+    const pdfData = await getPdfData(url);
+    const imageData = getPdfFirstPageImage(pdfData);
     const png = convertImageData(imageData);
     return new Response(png, {
       headers: {
@@ -26,6 +16,28 @@ export const handler: Handlers = {
     });
   },
 };
+
+async function getPdfData(url: string) {
+  const data = await fetch(url).then((res) => {
+    if (!res.ok) throw new Error(`Invalid url: ${res.statusText}`);
+    const contentType = res.headers.get("Content-Type");
+    if (contentType !== "application/pdf") {
+      throw new Error("Not a pdf document.");
+    }
+    return res.arrayBuffer();
+  });
+  return new Uint8Array(data);
+}
+
+function getPdfFirstPageImage(data: Uint8Array) {
+  const doc = new Document(data);
+  const page = doc.getPage(0);
+  const imageData = page?.getImageData();
+  page?.destroy();
+  doc.destroy();
+  if (!imageData) throw new Error("No Image found.");
+  return imageData;
+}
 
 function convertImageData(
   imageData: ImageData,
