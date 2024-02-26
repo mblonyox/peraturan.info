84a24298cc05389b98bbf967c9907527074c2863
Sukirno <mblonyox@gmail.com>
2024-02-26 21:55:21 +0700

    fix: error pages

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,17 +1,20 @@
-import { UnknownPageProps } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
 
-export default function NotFoundPage({ url }: UnknownPageProps) {
+export default function NotFoundPage({ url }: PageProps) {
   return (
-    <article>
-      <hgroup>
-        <h2>Error 404</h2>
-        <p>Laman tidak ditemukan: {url.pathname}</p>
-      </hgroup>
-      <a href="/">
-        <button className="outline">
+    <article className="text-center card">
+      <div className="card-body">
+        <h2 className="card-title">Error 404</h2>
+        <p className="card-text">
+          Laman tidak ditemukan: <code>{url.toString()}</code>
+        </p>
+        <p className="card-text">
+          Maaf, silakan cek lagi apakah tautan tersebut sudah benar.
+        </p>
+        <a href="/" className="btn btn-outline-secondary">
           Kembali ke beranda
-        </button>
-      </a>
+        </a>
+      </div>
     </article>
   );
 }
--- routes/_500.tsx
+++ routes/_500.tsx
@@ -1,17 +1,31 @@
-import { ErrorPageProps } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
 
-export default function Error500Page({ error, url }: ErrorPageProps) {
+export default function Error500Page(
+  { error, remoteAddr: { hostname, port, transport }, url }: PageProps,
+) {
+  const message = (error as Error).message ?? "Unknow error";
+  const remoteAddr = `${transport}://${hostname}:${port}`;
   return (
-    <article>
-      <hgroup>
-        <h2>Error 500</h2>
-        <p>Terjadi kesalahan : {(error as Error).message}</p>
-      </hgroup>
-      <a href={url.href}>
-        <button className="outline">
-          Muat ulang
-        </button>
-      </a>
+    <article className="text-center card">
+      <div className="card-body">
+        <h2 className="card-title">Error 500</h2>
+        <p className="card-text">
+          Terjadi kesalahan : <code>{message}</code>
+        </p>
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <div className="card bg-body-secondary text-start">
+              <div className="card-body">
+                <samp>
+                  URL : {url.toString()} <br />
+                  Remote Address : {remoteAddr} <br />
+                  Time : {new Date().toISOString()}
+                </samp>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   );
 }
