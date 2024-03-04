e6e6571a65bcf3f74c52df63850914a461db65ac
Sukirno <mblonyox@gmail.com>
2024-03-04 09:47:15 +0700

    fix: add Permanent Redirect status code

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -5,7 +5,7 @@ export const handler = (_req: Request, ctx: FreshContext) => {
   if (HOSTNAME && ctx.url.hostname !== HOSTNAME) {
     const url = new URL(ctx.url);
     url.hostname = HOSTNAME;
-    return Response.redirect(url);
+    return Response.redirect(url, 308);
   }
   return ctx.next();
 };
