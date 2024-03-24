89bcf61354c30ebcc2ba54f999b24f68d0ba2eca
Sukirno <mblonyox@gmail.com>
2024-03-25 05:36:20 +0700

    fix: redirect with 301

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -5,7 +5,7 @@ export const handler = (_req: Request, ctx: FreshContext) => {
   if (HOSTNAME && ctx.url.hostname !== HOSTNAME) {
     const url = new URL(ctx.url);
     url.hostname = HOSTNAME;
-    return Response.redirect(url, 308);
+    return Response.redirect(url, 301);
   }
   return ctx.next();
 };
