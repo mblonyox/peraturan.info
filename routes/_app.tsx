3610bfb4b1ba028308e0fcc20bd2d85919ab62ca
Sukirno <mblonyox@gmail.com>
2024-05-24 22:54:35 +0700

    remove service worker

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -78,7 +78,6 @@ export default defineApp<AppContext>((req, { Component, url, state }) => {
           <SeoTags />
           <Component />
         </AppContextProvider>
-        <script src="/app.js" />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
