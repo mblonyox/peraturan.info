fa229f7d76f6a14cdfb7b2b98c67e851c61c45c2
Sukirno <mblonyox@gmail.com>
2025-08-06 23:07:11 +0700

    fix: remove esbuild target requirement

--- dev.ts
+++ dev.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 import { Builder } from "fresh/dev";
 
-const builder = new Builder({ target: "safari12" });
+const builder = new Builder();
 
 if (Deno.args.includes("build")) {
   await builder.build();
