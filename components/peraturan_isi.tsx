ce77e92216fd6a341a4af9f80923359cc38d8bca
Sukirno <mblonyox@gmail.com>
2025-08-30 09:30:34 +0700

    fix: tambahkan disclaimer

--- components/peraturan_isi.tsx
+++ components/peraturan_isi.tsx
@@ -39,6 +39,41 @@ export default function PeraturanIsi({ md, path, html, prev, next }: Props) {
           </div>
         </aside>
         <div className="drawer-content">
+          <div
+            role="alert"
+            className="alert alert-warning alert-soft"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <div>
+              <h5 className="text-lg font-bold">Disclaimer</h5>
+              <p>
+                Dokumen peraturan ini ditampilkan sebagai hasil <i>parsing</i>
+                {" "}
+                semi-otomatis menggunakan teknologi OCR{"  "}
+                <i>(Optical Character Recognition)</i>.
+              </p>
+              <p>
+                Oleh karena itu, dimungkinkan terdapat perbedaan format,
+                penulisan, maupun kekeliruan teks dari dokumen aslinya.
+              </p>
+              <p>
+                Untuk keakuratan dan keabsahan, silakan merujuk pada dokumen
+                resmi/sumber asli peraturan tersebut.
+              </p>
+            </div>
+          </div>
           <div className="flex justify-between my-2">
             <label
               htmlFor="outline"
