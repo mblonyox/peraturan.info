995231b8e67af3a579d40feae510ee58bee4cc06
mblonyox <mblonyox@gmail.com>
2025-12-10 16:34:16 +0700

    refactor: Update navbar dropdown UI, remove Babel core, downgrade Orama, and upgrade DaisyUI.

--- components/layout_navbar.tsx
+++ components/layout_navbar.tsx
@@ -103,9 +103,24 @@ export default function LayoutNavbar({ url, theme }: Props) {
             <li>
               {menu
                 ? (
-                  <details className="dropdown">
-                    <summary>{text}</summary>
-                    <ul className="menu dropdown-content bg-base-300 rounded-box z-1 p-2 shadow-2xl">
+                  <div className="dropdown dropdown-start">
+                    <div tabIndex={0} role="button">
+                      <span className="mx-1">{text}</span>
+                      <svg
+                        width="12px"
+                        height="12px"
+                        className="inline-block h-2 w-2 fill-current opacity-60"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 2048 2048"
+                      >
+                        <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z">
+                        </path>
+                      </svg>
+                    </div>
+                    <ul
+                      tabIndex={-1}
+                      className="menu dropdown-content bg-base-300 rounded-box z-1 p-2 shadow-2xl"
+                    >
                       {menu.map(({ href, text }) => (
                         <li>
                           <a
@@ -118,7 +133,7 @@ export default function LayoutNavbar({ url, theme }: Props) {
                         </li>
                       ))}
                     </ul>
-                  </details>
+                  </div>
                 )
                 : (
                   <a
