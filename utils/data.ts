cd5b9589db0027f70769f577515a09dc6bc4b2be
mblonyox <mblonyox@gmail.com>
2026-01-09 11:39:15 +0700

    cache: getData

--- utils/data.ts
+++ utils/data.ts
@@ -4,62 +4,71 @@ interface PeraturanId {
   nomor: string | number;
 }
 
-export const getDatabaseBytes = async () => {
+async function cachedFetch(url: string | URL | Request) {
+  const cache = await caches.open("data");
+  const cachedResponse = await cache.match(url);
+  if (cachedResponse) return cachedResponse;
+  const response = await fetch(url);
+  await cache.put(url, response.clone());
+  return response;
+}
+
+export async function getDatabaseBytes() {
   try {
     const dataUrl = Deno.env.get("DATA_URL");
     if (!dataUrl) throw new Error("DATA_URL not found");
     const url = dataUrl + "/database.sqlite";
-    const response = await fetch(url);
+    const response = await cachedFetch(url);
     if (!response.ok) throw new Error("Failed to fetch database.sqlite");
     return response.bytes();
   } catch {
     return null;
   }
-};
+}
 
-export const getOramaDpackText = async () => {
+export async function getOramaDpackText() {
   try {
     const dataUrl = Deno.env.get("DATA_URL");
     if (!dataUrl) throw new Error("DATA_URL not found");
     const url = dataUrl + "/orama.dpack";
-    const response = await fetch(url);
+    const response = await cachedFetch(url);
     if (!response.ok) throw new Error("Failed to fetch orama.dpack");
     return response.text();
   } catch {
     return null;
   }
-};
+}
 
-export const getPeraturanMarkdown = async ({
+export async function getPeraturanMarkdown({
   jenis,
   tahun,
   nomor,
-}: PeraturanId) => {
+}: PeraturanId) {
   try {
     const dataUrl = Deno.env.get("DATA_URL");
     if (!dataUrl) throw new Error("DATA_URL not found");
     const url = [dataUrl, jenis, tahun, nomor, "fulltext.md"].join("/");
-    const response = await fetch(url);
+    const response = await cachedFetch(url);
     if (!response.ok) throw new Error("Failed to fetch fulltext.md");
     return response.text();
   } catch {
     return null;
   }
-};
+}
 
-export const getPeraturanThumbnail = async ({
+export async function getPeraturanThumbnail({
   jenis,
   tahun,
   nomor,
-}: PeraturanId) => {
+}: PeraturanId) {
   try {
     const dataUrl = Deno.env.get("DATA_URL");
     if (!dataUrl) throw new Error("DATA_URL not found");
     const url = [dataUrl, jenis, tahun, nomor, "thumbnail.png"].join("/");
-    const response = await fetch(url);
+    const response = await cachedFetch(url);
     if (!response.ok) throw new Error("Failed to fetch thumbnail.png");
     return response.bytes();
   } catch {
     return null;
   }
-};
+}
