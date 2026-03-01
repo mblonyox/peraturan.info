7eea6e8de259a690d559a787acdca16200531f53
Sukirno <mblonyox@gmail.com>
2026-03-01 17:43:47 +0700

    refactor: Modify secure headers by creating a new Headers object and returning a new Response.

--- utils/secure-headers.ts
+++ utils/secure-headers.ts
@@ -3,20 +3,21 @@ import { define } from "./define.ts";
 export const secureHeaders = () =>
   define.middleware(async (ctx) => {
     const response = await ctx.next();
-    response.headers.set(
+    const headers = new Headers(response.headers);
+    headers.set(
       "Strict-Transport-Security",
       "max-age=31536000; includeSubDomains; preload",
     );
-    response.headers.set(
+    headers.set(
       "Content-Security-Policy",
-      "default-src 'self'; script-src 'self' 'unsafe-inline' 'https://mouthful.inoxsegar.com'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://mouthful.inoxsegar.com; frame-ancestors 'none'; form-action 'none'; base-uri 'self';",
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'https://mouthful.inoxsegar.com'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' 'https://mouthful.inoxsegar.com'; frame-ancestors 'none'; form-action 'none'; base-uri 'self';",
     );
-    response.headers.set("X-Frame-Options", "DENY");
-    response.headers.set("X-Content-Type-Options", "nosniff");
-    response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
-    response.headers.set(
+    headers.set("X-Frame-Options", "DENY");
+    headers.set("X-Content-Type-Options", "nosniff");
+    headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+    headers.set(
       "Permissions-Policy",
       "geolocation=(), microphone=(), camera=(), payment=(), usb=(), gyroscope=(), accelerometer=(), magnetometer=(), speaker=(), fullscreen=(), interest-cohort=()",
     );
-    return response;
+    return new Response(response.body, { headers, status: response.status });
   });
