e9c1af509cf8c49d16aec0ad36c0f79f4dbc0615
mblonyox <mblonyox@gmail.com>
2026-03-04 12:31:36 +0700

    refactor: replace `Transform.toWeb` with a direct `TransformStream` constructor in `toTransformStream`.

--- utils/streams.ts
+++ utils/streams.ts
@@ -1,9 +1,5 @@
 import { Transform } from "node:stream";
 
-export function toTransformStream(transform: Transform) {
-  const { readable, writable } = Transform.toWeb(transform);
-  const transformStream = new TransformStream();
-  readable.pipeTo(transformStream.writable);
-  transformStream.readable.pipeTo(writable);
-  return transformStream;
-}
+export const toTransformStream = (transform: Transform) => {
+  return Transform.toWeb(transform) as TransformStream;
+};
