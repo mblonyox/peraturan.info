cde5cc25f42311c792fe393a698cff03f6d0cffb
Sukirno <mblonyox@gmail.com>
2026-04-25 12:54:09 +0700

    fix: update redirect path to be absolute to ensure correct routing in document handler

--- app/[jenis]/[tahun]/[nomor]/route.ts
+++ app/[jenis]/[tahun]/[nomor]/route.ts
@@ -6,7 +6,8 @@ import { getPeraturanData } from "./data";
 type Context = RouteContext<"/[jenis]/[tahun]/[nomor]">;
 
 export async function GET(_: NextRequest, { params }: Context) {
-  const { md } = await getPeraturanData(await params);
+  const { jenis, tahun, nomor } = await params;
+  const { md } = await getPeraturanData({ jenis, tahun, nomor });
   const subPath = md ? "isi" : "info";
-  return redirect(`./${subPath}`);
+  return redirect(`/${jenis}/${tahun}/${nomor}/${subPath}`);
 }
