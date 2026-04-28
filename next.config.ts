c1a1b5c10beede429cd2a8a84584859feeb78020
mblonyox <mblonyox@gmail.com>
2026-04-28 17:32:19 +0700

    chore: migrate to pnpm checks and wrap next config in a function to conditionally apply rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,54 +1,58 @@
-import type { NextConfig } from "next";
+import { type NextConfig } from "next";
+import { PHASE_PRODUCTION_SERVER } from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-        ],
-      },
-      {
-        source: "/search.xml",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/opensearchdescription+xml",
-          },
-        ],
-      },
-    ];
-  },
-  rewrites() {
-    return [
+export default function AppConfig(phase: string) {
+  const nextConfig: NextConfig = {
+    /* config options here */
+    output: "standalone",
+    headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            {
+              key: "X-Frame-Options",
+              value: "DENY",
+            },
+            {
+              key: "X-Content-Type-Options",
+              value: "nosniff",
+            },
+            {
+              key: "Referrer-Policy",
+              value: "strict-origin-when-cross-origin",
+            },
+            {
+              key: "Permissions-Policy",
+              value: "camera=(), microphone=(), geolocation=()",
+            },
+            {
+              key: "Strict-Transport-Security",
+              value: "max-age=63072000; includeSubDomains; preload",
+            },
+          ],
+        },
+        {
+          source: "/search.xml",
+          headers: [
+            {
+              key: "Content-Type",
+              value: "application/opensearchdescription+xml",
+            },
+          ],
+        },
+      ];
+    },
+  };
+
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    nextConfig.rewrites = () => [
       {
         source: "/api/mouthful/:path*",
         destination: `${process.env.MOUTHFUL_URL}/:path*`,
       },
     ];
-  },
-};
+  }
 
-export default nextConfig;
+  return nextConfig;
+}
