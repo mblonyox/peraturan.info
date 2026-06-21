26da9cdbd6b63893c2341437c1afa2ad10edd534
Sukirno <mblonyox@gmail.com>
2026-06-21 12:54:44 +0700

    refactor: replace inline SVG icons with reusable icon components

--- app/[jenis]/[[...tahun]]/page.tsx
+++ app/[jenis]/[[...tahun]]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { cache } from "react";
 import { z } from "zod";
 
+import IconFunnel from "@/components/icons/funnel";
 import Pagination from "@/components/pagination";
 import {
   getDB,
@@ -100,16 +101,7 @@ export default async function Page(props: Props) {
       <div className="container drawer-content my-5">
         <div className="block lg:hidden my-2">
           <label htmlFor="filters" className="btn" aria-label="Open Filters">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-funnel"
-              viewBox="0 0 16 16"
-            >
-              <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
-            </svg>
+            <IconFunnel />
             &nbsp;Saring
           </label>
         </div>
