aa2a7271adaed1436cca672608c057d63c39f33d
mblonyox <mblonyox@gmail.com>
2026-08-21 10:39:03 +0700

    refactor: inject peraturan.css as raw string into printd for consistent styling

--- src/components/partials/PrintButton.tsx
+++ src/components/partials/PrintButton.tsx
@@ -1,13 +1,15 @@
 import { Icon } from "astro-iconset/react";
 import { useCallback } from "react";
 
+import style from "@/styles/peraturan.css?raw";
+
 export default function PrintButton() {
   const onClickHandler = useCallback(async () => {
     const printd = await import("printd");
     const d = new printd.Printd();
     const el = document.querySelector(".peraturan");
     if (!el) return;
-    d.print(el as HTMLElement, ["/peraturan.css"]);
+    d.print(el as HTMLElement, [style]);
   }, []);
 
   return (
